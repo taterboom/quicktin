5ab0a66050d3e979450708113c5f334dc0d511e0
xueyong <1085523029@qq.com>
2022-08-12 23:56:50 +0800

    :tada: init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Quicktin
+
+> "Quick Tailwindcss in Nextjs" so called.
+> Add tailwindcss in nextjs project by one command.
+
+## Usage
+
+```sh
+npx quicktin # equal to "npx quicktin --pnpm"
+# or you can use --npm, --yarn
+```
---
+++ index.mjs
@@ -0,0 +1,27 @@
+import "zx/globals"
+import fs from "fs/promises"
+
+const INSTALL_COMMAND = {
+  npm: "install",
+  yarn: "add",
+  pnpm: "add",
+}
+
+const manager = process.argv[2] ? process.argv[2].slice(2) : "pnpm"
+
+echo("\u001b[36m>>>1. Add dependencies<<<")
+await $`${manager} ${INSTALL_COMMAND[manager]} -D tailwindcss postcss autoprefixer`
+
+echo("\u001b[36m>>>2. Init tainwindcss<<<")
+await $`npx tailwindcss init -p`
+
+echo("\u001b[36m>>>3. Update tailwind.config.js<<<")
+const { default: tailwindConfig } = await import("./tailwind.config.js")
+tailwindConfig.content.push("./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}")
+fs.writeFile(
+  "./tailwind.config.js",
+  `/** @type {import('tailwindcss').Config} */
+module.exports = ${JSON.stringify(tailwindConfig, undefined, 2)}`
+)
+
+echo("\u001b[32m>>>Done!<<<")
